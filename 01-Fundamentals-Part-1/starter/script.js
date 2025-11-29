56cd2007fbc029f9b35f3eda5fd4c4434956b28e
Manny <125616644+manny-the-great@users.noreply.github.com>
2025-11-29 13:01:54 +0100

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+console.log(48 + 8 + 23 - 10);
+
+console.log("Manny");
+console.log(23);
+
+//VARIABLES: A box that is used to store a value, it takes the variable name and the value assigned to the variable.
+// A variable name can only take camel-casing (firstName), numbers (manny3), dollar-sign ($testing), and underscores (my_source_code).
+
+let firstName = "Manny";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let manny_johnson = 'Manny Johnson';
+let $functionCode = 27;
+let PI = 3.1415; //Variable names written in uppercase are for constants that will never change
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
