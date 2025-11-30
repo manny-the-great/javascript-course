190820c11c1dbd39485e4950ae5d8427ad5ff6f8
Emmanuel Johnson <leunammejohnson17@gmail.com>
2025-11-30 09:15:27 +0100

    Just finished learning Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/* let js = "amazing";
 console.log(48 + 8 + 23 - 10);
 
 console.log("Manny");
@@ -19,25 +19,30 @@ let PI = 3.1415; //Variable names written in uppercase are for constants that wi
 let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
-console.log(myFirstJob);
+console.log(myFirstJob); */
 
 // Data Types
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// Number
+let age = 23;
+// Strings
+let fullName = "Manny Johnson";
+//Boolean = A true or false value
+let isActive = true;
+console.log(isActive);
+// Undefined = An empty value that is not defined
+let jobDescription;
+console.log(typeof jobDescription);
+// Null
+console.log(typeof null); // This is a bug in JavaScript, it should return 'object' 
+// Symbol (ES2015)
+//BigInt (ES2020)
+// Typeof Operator - Used to check the data type of a variable
+console.log(typeof isActive);
+console.log(typeof fullName);
+console.log(typeof age);
 
-// console.log(typeof true);
-console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
-
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
-
-let year;
-console.log(year);
-console.log(typeof year);
-
-year = 1991;
-console.log(typeof year);
-
-console.log(typeof null);
+// Dynamic Typing - The ability of a programming language to change data types of a variable at runtime
+age = "Twenty Three";
+console.log(typeof age);
+jobDescription = "Blockchanin Developer";
+console.log(typeof jobDescription);
